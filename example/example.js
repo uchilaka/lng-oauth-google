89f5779780ae283c60fc1b7c4974c8bcc09d6114
Uchenna Chilaka <ndokoba@gmail.com>
2016-05-29 03:55:54 -0400

    Nothing to see here. Just updating...

---
+++ example/example.js
@@ -0,0 +1,9 @@
+angular.module('app.example', []).controller('HelloAppCtrl', [
+    '$scope', function ($scope) {
+        console.log('[module.HelloAppCtrl]');
+        
+        $scope.login = function() {
+            
+        };
+        
+    }]);
